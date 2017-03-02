6a771b86499880497b154ad5bd08eb35a03ca654
Kalvim Scotti <kst@certi.org.br>
2017-03-02 16:03:47 -0300

    adjust config

--- config/easyii.php
+++ config/easyii.php
@@ -6,18 +6,8 @@ return [
             'class' => 'yii\easyii\AdminModule',
         ],
         'markdown' => [
-            // the module class
             'class' => 'kartik\markdown\Module',
-
-            // the controller action route used for markdown editor preview
             'previewAction' => '/markdown/parse/preview',
-
-            // the list of custom conversion patterns for post processing
-            'customConversion' => [
-                '<table>' => '<table class="table table-bordered table-striped">'
-            ],
-
-            // whether to use PHP SmartyPantsTypographer to process Markdown output
             'smartyPants' => true,
         ],
     ],
